01a65ac86cad0c44c6a857850fbc01f5b4215938
Bruno de Almeida Ferreira <bruno.bdaferreira@gmail.com>
2018-09-17 14:05:00 -0300

    adicionando opcao de remover os campos de titulo

--- app/helpers/md_simple_editor/rails/md_helper.rb
+++ app/helpers/md_simple_editor/rails/md_helper.rb
@@ -2,7 +2,8 @@ module MdSimpleEditor
   module Rails
     module MdHelper
       def markdown_editor(form, field, klass = '', options = {})
-        render partial: 'md_simple_editor/editor_tool_bar', locals: { form: form, field: field, options: options }
+        default_options = { show_titles: true }
+        render partial: 'md_simple_editor/editor_tool_bar', locals: { form: form, field: field, options: default_options.merge(options) }
       end
     end
   end
